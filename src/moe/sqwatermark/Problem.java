d81f086a59a8197133f46eb2e3c7256aaebd2376
SQwatermark <sqwatermark@outlook.com>
2020-06-22 16:38:19 +0800

    dca

--- src/moe/sqwatermark/Problem.java
+++ src/moe/sqwatermark/Problem.java
@@ -9,9 +9,14 @@ public class Problem {
     public static Logger logger = Logger.getLogger("three-body");
 
     public static void main(String[] args) {
-        Population population = Population.getNewPopulation(20).evolution(50);
-        logger.info("每一代的最大适应度为" + population.maxValues.toString());
-        logger.info("所有代中最大适应度为" + Collections.max(population.maxValues));
+//        Population population = Population.getNewPopulation(20).evolution(50);
+//        logger.info("每一代的最大适应度为" + population.maxValues.toString());
+//        logger.info("所有代中最大适应度为" + Collections.max(population.maxValues));
+
+    }
+
+    public static double[] decodeFromResult(String s) {
+
     }
 
 }
